ff921499c32d423fc44f4b5beeddab58f3ad9e45
Carl Joseph <carljoseph156@gmail.com>
2025-07-29 09:38:52 -0400

    Added/Updated types

---
+++ lib/types.ts
@@ -0,0 +1,15 @@
+export interface User {
+  id: string;
+  username: string;
+  avatar: string;
+}
+
+export interface Post {
+  id: string;
+  user_id: string;
+  parent_id?: string | null;
+  text: string;
+  created_at?: string;
+  User?: User;
+  Post?: Post[];
+}
