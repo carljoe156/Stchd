b31d857b42491299abb5296677f12523677012b8
Carl Joseph <carljoseph156@gmail.com>
2025-07-30 23:58:40 -0400

    Added/Updated App Layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout() {
               <Stack.Screen name="camera" options={{ headerShown: false, presentation: "modal" }} />
               <Stack.Screen name="gif" options={{ headerShown: false, presentation: "modal" }} />
               <Stack.Screen name="places" options={{ headerShown: false, presentation: "modal" }} />
-              {/* <Stack.Screen name="thread" options={{ headerShown: false }} /> */}
+              <Stack.Screen name="thread" options={{ headerShown: false }} />
               <Stack.Screen name="+not-found" />
             </Stack>
           </PostProvider>
