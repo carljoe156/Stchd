9a41e373aa6828bf8ee4e1af663d1e6295a17371
Carl Joseph <carljoseph156@gmail.com>
2025-08-01 23:58:06 -0400

    Added/Updated User Profile

--- components/shared/user.tsx
+++ components/shared/user.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Pressable, SafeAreaView } from "react-native";
+import { FlatList, Pressable, SafeAreaView, Share } from "react-native";
 import { Text } from "@/components/ui/text";
 import { Button, ButtonText } from "@/components/ui/button";
 import { useAuth } from "@/providers/AuthProvider";
@@ -9,6 +9,10 @@ import { Avatar, AvatarBadge, AvatarFallbackText, AvatarImage } from "@/componen
 import { Plus } from "lucide-react-native";
 import * as ImagePicker from "expo-image-picker";
 import { usePost } from "@/providers/PostProvider";
+import { usePosts } from "@/hooks/use-posts";
+import { VStack } from "@/components/ui/vstack";
+import PostView from "@/components/shared/post-view";
+import BottomSheet from "@/components/shared/bottom-sheet";
 
 enum Tab {
   THREADS = "Threads",
@@ -16,11 +20,28 @@ enum Tab {
   REPOSTS = "Reposts",
 }
 
+const Tabs = [
+  {
+    name: Tab.THREADS,
+    key: "user_id",
+  },
+  {
+    name: Tab.REPLIES,
+    key: "user_id",
+  },
+  {
+    name: Tab.REPOSTS,
+    key: "repost_user_id",
+  },
+];
+
 export default ({ user }: { user: User }) => {
   const { logOut } = useAuth();
-  const [tab, setTab] = React.useState<Tab>(Tab.THREADS);
+  const [tab, setTab] = React.useState<(typeof Tabs)[number]>(Tabs[0]);
   //    const [photo, setPhoto] = React.useState<String>('');
-  const avatarUrl = `${process.env.EXPO_PUBLIC_BUCKET_URL}/${user.id}/avatar.jpeg`;
+  const avatarUrl = `${process.env.EXPO_PUBLIC_BUCKET_URL}/${user?.id}/avatar.jpeg`;
+  const { data, isLoading, refetch } = usePosts({ key: tab.key, value: user?.id, type: "eq" });
+  const [showActionSheet, setShowActionSheet] = React.useState(false);
   const { uploadFile } = usePost();
 
   const addPhoto = async () => {
@@ -43,8 +64,28 @@ export default ({ user }: { user: User }) => {
     console.log(path);
   };
 
+  //   const addPhoto = async () => {
+  //   const result = await pickImage();
+  //   if (!result) return;
+
+  //   const { uri, type } = result;
+
+  //   await uploadFile({
+  //     userId: authUser?.id || '',
+  //     uri,
+  //     type,
+  //     name: 'avatar.jpeg',
+  //   });
+  // };
+
+  const shareProfile = async () => {
+    await Share.share({
+      message: `Check out ${user?.username}'s profile on Post!`,
+    });
+  };
+
   return (
-    <SafeAreaView className="flex-1 pt-10">
+    <SafeAreaView className="flex-1 pt-10 bg-white">
       <HStack className="items-center justify-between p-3">
         <Text size="3xl" bold className="text-black">
           {user?.username}
@@ -66,26 +107,47 @@ export default ({ user }: { user: User }) => {
         {/* <Text size="xl" bold className="text-black">
           {user?.bio}
         </Text> */}
-        <Button onPress={() => {}} variant="outline" className="flex-1 border-gray-400 rounded-xl">
+        <Button
+          onPress={() => setShowActionSheet(true)}
+          variant="outline"
+          className="flex-1 border-gray-400 rounded-xl"
+        >
           <ButtonText>Edit profile</ButtonText>
         </Button>
-        <Button onPress={() => {}} variant="outline" className="flex-1 border-gray-400 rounded-xl">
+        <Button
+          onPress={shareProfile}
+          variant="outline"
+          className="flex-1 border-gray-400 rounded-xl"
+        >
           <ButtonText>Share profile</ButtonText>
         </Button>
       </HStack>
 
       <HStack className="items-center justify-between p-3">
-        {Object.values(Tab).map((t) => (
+        {Tabs.map((t) => (
           <Button
-            key={t}
+            key={t.name}
             onPress={() => setTab(t)}
             variant="link"
-            className={`flex-1 border-b ${t === tab ? "border-black" : "border-gray-300"}`}
+            className={`flex-1 border-b ${
+              t.name === tab.name ? "border-black" : "border-gray-400"
+            }`}
           >
-            <ButtonText className={t === tab ? "text-black" : "text-gray-500"}>{t}</ButtonText>
+            <ButtonText className={`${t.name === tab.name ? "text-black" : "text-gray-500"}`}>
+              {t.name}
+            </ButtonText>
           </Button>
         ))}
       </HStack>
+      <VStack space="lg">
+        <FlatList
+          data={data}
+          refreshing={isLoading}
+          onRefresh={refetch}
+          renderItem={({ item }) => <PostView item={item} refetch={refetch} />}
+        />
+      </VStack>
+      <BottomSheet showActionSheet={showActionSheet} setShowActionSheet={setShowActionSheet} />
     </SafeAreaView>
   );
 };
