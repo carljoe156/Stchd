606eae39008528782069b39e91f15cec3eafcd90
Carl Joseph <carljoseph156@gmail.com>
2025-08-01 23:45:40 -0400

    Added/Updated Use Posts hook

--- hooks/use-posts.ts
+++ hooks/use-posts.ts
@@ -1,21 +1,34 @@
 import { useQuery } from "@tanstack/react-query";
 import { supabase } from "@/lib/supabase";
 
-export const getPosts = async () => {
+interface PostProps {
+  key: string;
+  value: string | null;
+  type: "eq" | "is";
+}
+
+export const getPosts = async ({ key, value, type }: PostProps) => {
   const { data, error } = await supabase
     .from("Post")
     .select(
-      "*, Place(name), Like(user_id), repost_user:User!repost_user_id(*), user:User!user_id(*)"
+      `
+    *,
+    user:User!user_id(*),
+    Place(name),
+    Like(user_id),
+    repost_user:User!repost_user_id(*),
+    Post(*, user:User!user_id(*))
+    `
     )
-    .is("parent_id", null)
+    .filter(key, type, value)
     .order("created_at", { ascending: false });
   if (!error) return data;
 };
 
-export const usePosts = () => {
+export const usePosts = ({ key, value, type }: PostProps) => {
   const { data, isLoading, error, refetch } = useQuery({
-    queryKey: ["posts"],
-    queryFn: getPosts,
+    queryKey: ["posts", key, type, value],
+    queryFn: () => getPosts({ key, value, type }),
   });
 
   return { data, isLoading, error, refetch };
