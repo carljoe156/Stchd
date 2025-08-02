58be8ed94930afe9fcfd31b770a62f3d93ab7250
Carl Joseph <carljoseph156@gmail.com>
2025-08-01 23:44:18 -0400

    Added/Updated Use Threads hook

--- hooks/use-thread.ts
+++ hooks/use-thread.ts
@@ -1,21 +1,22 @@
-import { useQuery } from "@tanstack/react-query";
-import { supabase } from "@/lib/supabase";
+// MERGED WITH USEPOSTS HOOK
+// import { useQuery } from "@tanstack/react-query";
+// import { supabase } from "@/lib/supabase";
 
-export const getThread = async (id: string) => {
-  const { data, error } = await supabase
-    .from("Post")
-    .select("*, user:User!user_id(*), Place(name), Post(*, user:User!user_id(*))")
-    .eq("id", id)
-    // .filter('id', 'eq', id)
-    .order("created_at", { ascending: false });
-  if (!error) return data;
-};
+// export const getThread = async (id: string) => {
+//   const { data, error } = await supabase
+//     .from("Post")
+//     .select("*, user:User!user_id(*), Place(name), Post(*, user:User!user_id(*))")
+//     // .eq("id", id)
+//     .filter("id", "eq", id)
+//     .order("created_at", { ascending: false });
+//   if (!error) return data;
+// };
 
-export const useThread = (id: string) => {
-  const { data, isLoading, error, refetch } = useQuery({
-    queryKey: ["thread"],
-    queryFn: () => getThread(id),
-  });
+// export const useThread = (id: string) => {
+//   const { data, isLoading, error, refetch } = useQuery({
+//     queryKey: ["thread"],
+//     queryFn: () => getThread(id),
+//   });
 
-  return { data, isLoading, error, refetch };
-};
+//   return { data, isLoading, error, refetch };
+// };
