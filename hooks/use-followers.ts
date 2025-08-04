9f42c713aeb5822050a43190f997fba6d415a2d9
Carl Joseph <carljoseph156@gmail.com>
2025-08-04 12:37:40 -0400

    Added/Updated Use Followers hook

---
+++ hooks/use-followers.ts
@@ -0,0 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
+import { supabase } from "@/lib/supabase";
+// following_user: User!following_user_id(*)
+
+export const getFollowers = async (userId: string) => {
+  if (!userId) return null;
+  const { data, error } = await supabase
+    .from("Followers")
+    .select(
+      `
+        *, 
+        user: User!user_id(*)
+         `
+    )
+    .eq("following_user_id", userId);
+
+  // if (!error) return followers
+  if (!error) return data;
+};
+
+export const useFollowers = (userId: string) => {
+  const { data, isLoading, error, refetch } = useQuery({
+    queryKey: ["followers", userId],
+    queryFn: () => getFollowers(userId),
+  });
+
+  return { data, isLoading, error, refetch };
+};
