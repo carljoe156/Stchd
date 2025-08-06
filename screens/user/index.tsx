70be817393ce2b9155d4e80ca90a3fd3bf7a0de6
Carl Joseph <carljoseph156@gmail.com>
2025-08-05 23:52:40 -0400

    Added/Updated User

--- screens/user/index.tsx
+++ screens/user/index.tsx
@@ -1,9 +1,9 @@
-import User from "@/components/shared/user";
+import User from "@/components/shared/profile-view";
 import { useLocalSearchParams } from "expo-router";
 import { useUser } from "@/hooks/use-user";
 
 export default () => {
   const { userId } = useLocalSearchParams();
-  const { data: user } = useUser(userId as string);
+  const { data: user } = useUser({ userId: userId as string });
   return <User user={user} />;
 };
