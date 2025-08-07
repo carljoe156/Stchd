e2203c571814532ab421fba9b0f4c270fec9e8c6
Carl Joseph <carljoseph156@gmail.com>
2025-08-06 21:51:28 -0400

    Added/Updated App Layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,13 @@
 import { useFonts } from "expo-font";
 import "@/global.css";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
-import { Stack } from "expo-router";
+import { router, Stack } from "expo-router";
 import React from "react";
 import { AuthProvider } from "@/providers/AuthProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PostProvider } from "@/providers/PostProvider";
+// import { ChevronLeft } from "lucide-react-native";
+// import { Pressable } from "react-native";
 
 const queryClient = new QueryClient();
 
@@ -24,7 +26,18 @@ export default function RootLayout() {
       <QueryClientProvider client={queryClient}>
         <AuthProvider>
           <PostProvider>
-            <Stack initialRouteName="(auth)">
+            <Stack
+              initialRouteName="(auth)"
+              // screenOptions={{
+              //   headerTransparent: true,
+              //   headerLeft: ({ canGoBack }) => (
+              //     <Pressable onPress={canGoBack ? () => router.back() : undefined}>
+              //       <ChevronLeft color="#000" />
+              //     </Pressable>
+              //   ),
+              // }}
+            >
+              {/* <Stack> */}
               <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
               <Stack.Screen name="(auth)" options={{ headerShown: false }} />
               <Stack.Screen name="post" options={{ headerShown: false, presentation: "modal" }} />
@@ -32,7 +45,8 @@ export default function RootLayout() {
               <Stack.Screen name="gif" options={{ headerShown: false, presentation: "modal" }} />
               <Stack.Screen name="places" options={{ headerShown: false, presentation: "modal" }} />
               <Stack.Screen name="thread" options={{ headerShown: false }} />
-              <Stack.Screen name="user" />
+              <Stack.Screen name="posts" options={{ title: "Posts" }} />
+              <Stack.Screen name="user" options={{ title: "" }} />
               <Stack.Screen name="+not-found" />
             </Stack>
           </PostProvider>
