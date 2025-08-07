9ef35bb23d1cbd6438c5e251a4281852ea27b983
Carl Joseph <carljoseph156@gmail.com>
2025-08-06 21:32:19 -0400

    Added/Updated Use Place hook

---
+++ hooks/use-places.ts
@@ -0,0 +1,31 @@
+import { supabase } from "@/lib/supabase";
+import { useQuery } from "@tanstack/react-query";
+
+const getPlaces = async () => {
+  const { data, error } = await supabase
+    .from("Place")
+    .select("*")
+    .order("created_at", { ascending: false })
+    .limit(10);
+  if (error) throw error;
+  return data;
+};
+
+export const searchPlaces = async (search: string) => {
+  const { data, error } = await supabase
+    .from("Place")
+    .select("*")
+    .ilike("name", `%${search}&`)
+    .order("created_at", { ascending: false });
+  if (error) throw error;
+  return data;
+};
+
+export const usePlaces = (search: string) => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["places", search],
+    queryFn: search ? () => searchPlaces(search) : getPlaces,
+  });
+
+  return { data, isLoading, error };
+};
